2459836b685cc6f5ce32f099182a3a69db22ca33
Vidar Hagen <vidarh@hotmail.com>
2019-02-14 11:41:50 +0100

    Add project files.

---
+++ Visma.Training.Serverless.ServiceBus/SendTopic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Visma.Training.Serverless.ServiceBus
+{
+        public  class SendTopic
+        {
+            public static string ServiceBusConnectionString = "";
+            public static  string TopicName = "";
+            public static string Message = "";
+            public static int NumberOfMessages = 1;
+            static ITopicClient topicClient;
+
+        public static void Send(string cstr, string topicname, string messasge, int numberOfMessages)
+        {
+            ServiceBusConnectionString = cstr;
+            TopicName = topicname;
+            Message = messasge;
+            NumberOfMessages = numberOfMessages;
+
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+
+            static async Task MainAsync()
+            {
+            
+                topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+             
+                // Send messages.
+                await SendMessagesAsync(NumberOfMessages);
+
+                await topicClient.CloseAsync();
+            }
+
+            static async Task SendMessagesAsync(int numberOfMessagesToSend)
+            {
+                try
+                {
+                    for (var i = 0; i < numberOfMessagesToSend; i++)
+                    {
+                        // Create a new message to send to the topic
+                        string messageBody = Message + " " + i;
+                        var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                        // Send the message to the topic
+                        await topicClient.SendAsync(message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+
+
---
+++ WebApplication22/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Visma.Training.Serverless.ServiceBus;
+
+namespace WebApplication22.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            var serviceBus = Request.Query["sb"];
+            var topicname = Request.Query["topic"];
+            var num = 0;
+            try
+            {
+                num = Int32.Parse(Request.Query["num"]);
+            }
+            catch (Exception)
+            {
+                num = 10;
+            }
+
+            var txt = Request.Query["content"];
+
+            if (num > 50)
+                num = 50;
+
+            if (!string.IsNullOrEmpty(serviceBus))
+            {
+               SendTopic.Send(serviceBus, topicname, txt, num);
+             }
+
+        }
+ 
+    }
+}
